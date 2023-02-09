13903698e2acf654f1f8188717bf137c2d2ab86b
Nishgaur <84866296+Nishgaur@users.noreply.github.com>
2023-02-09 20:40:55 +0530

    Create functions.go

---
+++ functions.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+/*
+func func_name(){
+	multiple statements
+	}*/
+/*func main() {
+	a()
+}
+func a() {
+	fmt.Print(12)
+	b()
+}
+func b() {
+	c()
+}
+func c() {
+
+}
+*/
+func main()
+result, x := c()
+fmt.println(result)
+fmt.println(x)
+r, :=b(1, 2, 3, 4, 5, 6, 6)
+fmt.print(r)
+▸
+func a() (int, string) {
+return 122, "weqrr3"
+}
+func b(args ...int) (bool, int {
+for, v= range args {
+fmt.println(v)
+}
+return true, 11
+}
+func c() (i int, j string) {
+
+}
+func c() (i int, j string) {
+	i = 10
+	j = "hguytf"
+	return
+}
